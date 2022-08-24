f267981e8e2e55d2224f7a718107b7541cd3490c
Auca Maillot <aucacoyan@gmail.com>
2022-08-24 11:16:09 -0300

    add image and banner in home

--- real-state-site/components/Banner.jsx
+++ real-state-site/components/Banner.jsx
@@ -1,3 +1,5 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+
 export default function Banner({
   purpose,
   title1,
@@ -10,7 +12,22 @@ export default function Banner({
 }) {
   return (
     <Flex flexWrap="wrap" justifyContent="center">
-      {" "}
+      <img src={imageUrl} alt="" />
+      <Box p="5">
+        <Text color="gray.500" fontSize="sm" fontWeight="medium">
+          {purpose}
+        </Text>
+        <Text fontSize="3xl" fontWeight="bold">
+          {title1}
+          <br />
+          {title2}
+        </Text>
+        <Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700">
+          {desc1}
+          <br />
+          {desc2}
+        </Text>
+      </Box>
     </Flex>
   );
 }
--- real-state-site/components/Home.jsx
+++ real-state-site/components/Home.jsx
@@ -1,9 +1,19 @@
 import { Box } from "@chakra-ui/react";
+import Banner from "./Banner";
+import image from "../assets/images/house.jpg";
 
 export default function Home({}) {
   return (
     <>
-      <Box color="#f00">this is a big red text</Box>
+      <Box>this is a big red text</Box>
+      <Banner
+        purpose="RENT A HOME"
+        title1="Rental Homes for"
+        title2="Everyone"
+        desc1=" Explore from Apartments, builder floors, villas"
+        desc2="and more"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+      />
     </>
   );
 }
