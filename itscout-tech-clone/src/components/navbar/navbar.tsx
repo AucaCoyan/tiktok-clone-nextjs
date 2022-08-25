465820f216f56f8f2659db0ca02d0212fa22fa6d
Auca Maillot <aucacoyan@gmail.com>
2022-08-25 17:39:16 -0300

    more work on navbar

--- itscout-tech-clone/src/components/navbar/navbar.tsx
+++ itscout-tech-clone/src/components/navbar/navbar.tsx
@@ -2,28 +2,26 @@ import Navlink from "./navlink";
 import NavLogo from "./navlogo";
 
 export default function Navbar() {
-    return (
-        <>
-    <div className="bg-deepblue flex text-white align-middle">
-        <p>in Whatsapp </p>
-        <div className="snap-center">
-            <ul>
-                <Navlink title='home' destination='#' />
-            <li>
-                <Navlink title='what we do' destination='#' />
-            </li>
-            <li>
-                <NavLogo />
-            </li>
-            <li>
-                <Navlink title='blog' destination='#' />
-            </li>
-            <li>
-                <Navlink title='contact us' destination='#' />
-            </li>
-            </ul>
-       </div>
-    </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex justify-between bg-deepblue text-white align-middle">
+        in Whatsapp
+        <ul>
+          <Navlink title="home" destination="#" />
+          <li>
+            <Navlink title="what we do" destination="#" />
+          </li>
+          <li>
+            <NavLogo />
+          </li>
+          <li>
+            <Navlink title="blog" destination="#" />
+          </li>
+          <li>
+            <Navlink title="contact us" destination="#" />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
--- itscout-tech-clone/src/components/navbar/navlogo.tsx
+++ itscout-tech-clone/src/components/navbar/navlogo.tsx
@@ -1,8 +1,10 @@
-export default function NavLogo({ }) {
+export default function NavLogo({}) {
   return (
-  <img className=""
-  width={285}
-  height={104}
-  src="https://itscout.tech/wp-content/uploads/2022/05/Logo_header.svg" />
-    )
+    <img
+      className="cursor-pointer w-[150px] h-[65px]"
+      width={285}
+      height={104}
+      src="https://itscout.tech/wp-content/uploads/2022/05/Logo_header.svg"
+    />
+  );
 }
