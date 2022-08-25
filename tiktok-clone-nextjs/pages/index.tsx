3a50ab331a03dce7705fb564d4d3ab94f77acca5
Auca Maillot <aucacoyan@gmail.com>
2022-08-25 17:38:52 -0300

    more work on navbar and footer

--- tiktok-clone-nextjs/pages/index.tsx
+++ tiktok-clone-nextjs/pages/index.tsx
@@ -1,12 +1,8 @@
-import type { NextPage } from 'next'
-
+import type { NextPage } from "next";
+import axios from "axios";
 
 const Home: NextPage = () => {
-  return (
-    <h1 className="text-3x1 font-bold underline">
-      TikTik
-   </h1>
-  )
-}
+  return <h1 className="text-3x1 font-bold underline">TikTik</h1>;
+};
 
-export default Home
+export default Home;
