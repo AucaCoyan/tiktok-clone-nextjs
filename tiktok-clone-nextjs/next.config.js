402b13d90210d5a0711b3d1a7556b6317ff9cf11
Auca Maillot <aucacoyan@gmail.com>
2022-08-26 09:51:24 -0300

    finished VideoCard component

--- tiktok-clone-nextjs/next.config.js
+++ tiktok-clone-nextjs/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "www.spectrumcollections.com",
+      "images.unsplash.com",
+      "media-exp1.licdn.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
