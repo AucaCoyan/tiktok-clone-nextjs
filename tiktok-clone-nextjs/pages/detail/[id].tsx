23b05a86b1c92a11e55a33403cb5c4eae99b78b8
Auca <forosypaginas@gmail.com>
2022-08-29 11:35:54 -0300

    add details in video view and VideoCard Component

--- tiktok-clone-nextjs/pages/detail/[id].tsx
+++ tiktok-clone-nextjs/pages/detail/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import { GoVerified } from "react-icons/go";
@@ -8,6 +8,7 @@ import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
 import axios from "axios";
 import { BASE_URL } from "../../utils";
 import { Video } from "../../types";
+import Link from "next/link";
 
 interface IPropos {
   postDetails: Video;
@@ -16,6 +17,8 @@ interface IPropos {
 const Detail = ({ postDetails }: IPropos) => {
   const [post, setPost] = useState(postDetails);
   const [playing, setPlaying] = useState(false);
+  const [isVideoMuted, setIsVideoMuted] = useState(false);
+  const router = useRouter();
 
   const videoRef = useRef<HTMLVideoElement>(null);
 
@@ -29,13 +32,18 @@ const Detail = ({ postDetails }: IPropos) => {
     }
   };
 
+  useEffect(() => {
+    if (post && videoRef?.current) {
+      videoRef.current.muted = isVideoMuted;
+    }
+  }, [isVideoMuted]);
   if (!post) return null;
 
   return (
     <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
       <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black"></div>
       <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
-        <p>
+        <p className="cursor-pointer" onClick={router.back}>
           <MdOutlineCancel className="text-white text-[35px]" />
         </p>
       </div>
@@ -45,7 +53,7 @@ const Detail = ({ postDetails }: IPropos) => {
             src={post.video.asset.url}
             ref={videoRef}
             loop
-            onClick={() => {}}
+            onClick={onVideoClick}
           ></video>
         </div>
         <div className="absolute top-[45%] left-[45%] cursor-pointer">
@@ -54,6 +62,51 @@ const Detail = ({ postDetails }: IPropos) => {
               <BsFillPlayFill className="text-white text-xl lg:text-8xl" />
             </button>
           )}
+        </div>
+      </div>
+      <div className="absolute bottom-5 lg:bottom-10 right-5 lg:right-10 cursor-pointer">
+        {isVideoMuted ? (
+          <button onClick={() => setIsVideoMuted(false)}>
+            <HiVolumeOff className="text-white text-2xl ld:text-4xl" />
+          </button>
+        ) : (
+          <button onClick={() => setIsVideoMuted(true)}>
+            <HiVolumeUp className="text-white text-2xl ld:text-4xl" />
+          </button>
+        )}
+      </div>
+      <div className="relative w-[1000px] md:w-[900px] lg:w-[700px]">
+        <div className="lg:mt-20 mt-10">
+          <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+            <div className="md:w-16 md:h-16 w-10 h-10">
+              <Link href="/">
+                <>
+                  <Image
+                    width={62}
+                    height={62}
+                    className="rounded-full"
+                    src={post.postedBy.image}
+                    alt="profile phoot"
+                    layout="responsive"
+                  />
+                </>
+              </Link>
+            </div>
+            <div>
+              <Link href="/">
+                <div className="flex items-center gap-2 ">
+                  <p className="flex gap-2 items-center md:text-md font-bold text-primary">
+                    {post.postedBy.userName}
+                    {` `}
+                    <GoVerified className="text-blue-400 text-md" />
+                  </p>
+                  <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                    {post.postedBy.userName}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
